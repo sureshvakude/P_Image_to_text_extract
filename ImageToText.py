e49820424abb75ab5570d838afe10837088aee98
suresh.vakude <suresh.vakude@acty-sys.co.in>
2024-03-04 18:06:52 +0530

    Rev[1.0]

---
+++ ImageToText.py
@@ -0,0 +1,15 @@
+import pytesseract
+from PIL import Image
+
+# path of the installed tesseract.exe 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# image path
+image_path = "image.jpeg"
+image = Image.open(image_path)
+
+extracted_text = pytesseract.image_to_string(image)
+cleaned_text = extracted_text.strip()
+
+print("Extracted text:")
+print(cleaned_text)
---
+++ README.md
@@ -0,0 +1,7 @@
+# Steps to execute code
+
+Step 1. Install Tesseract on your machine
+        For installation use link -> https://github.com/UB-Mannheim/tesseract/wiki
+Step 2. Install Tesseract OCR and Required Libraries
+        Libraries like -> pytesseract , pillow
+Step 3. Copy the code and run
